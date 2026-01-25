3aaa469c6b988a3c54014eb7e3b96facc4985f3b
tris5572 <68403260+tris5572@users.noreply.github.com>
2026-01-26 05:17:44 +0900

    feat(pages): 文字列ケース変換ページの大枠を実装

--- src/components/pages/CaseConverter.tsx
+++ src/components/pages/CaseConverter.tsx
@@ -1,6 +1,27 @@
+import styles from "./CaseConverter.module.css";
+import { TextInput } from "../commons/TextInput";
+import { useCallback } from "react";
+import { ColorArrow } from "../commons/DownArrow";
+
 /**
  * 文字列のケースを変換するページ
  */
 export function CaseConverter() {
-  return <div>ケース変換</div>;
+  const onChange = useCallback((value: string) => {
+    console.log(value);
+  }, []);
+
+  return (
+    <div className={styles.wrapper}>
+      <h2>ケース変換</h2>
+      <TextInput
+        size={60}
+        font="monospace"
+        placeholder="文字列を入力"
+        onChange={onChange}
+      />
+      <ColorArrow />
+      <div>出力</div>
+    </div>
+  );
 }
